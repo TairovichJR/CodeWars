de2ac872abb1be3abda8f89e273219f092b02bbd
Omonjon Yokubov <yokubov.omonjon@gmail.com>
2020-07-27 01:35:56 -0400

    Halving the sum problem

---
+++ src/main/java/com/tairovich/kata_7/HalvingSum.java
@@ -0,0 +1,18 @@
+package com.tairovich.kata_7;
+
+public class HalvingSum {
+
+	public static int halvingSum(int n) {
+
+		int sum = n;
+		
+		while(n > 0) {		
+			n = n / 2;
+			sum += n;
+		}
+		
+		return sum;
+	}
+
+
+}
